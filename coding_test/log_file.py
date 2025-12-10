5fa82c520fc430ee402c8d4a318d91d4c87aa2f1
happysubin <asb0711@gmail.com>
2025-12-10 22:26:29 +0900

    주석 추가

--- coding_test/log_file.py
+++ coding_test/log_file.py
@@ -11,6 +11,7 @@ def sort(list: list[str]) -> list[str]:
             letters.append(log)    
 
     # 2. 문자 로그만 정렬
+    #                           1차 정렬 기준      2차 정렬 기준
     letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
 
     # 3. 문자 로그 + 숫자 로그(원래 순서) 반환
