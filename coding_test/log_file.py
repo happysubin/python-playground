e5996a4bc68024daa4bac9ca4c4506772f2dc2ef
happysubin <asb0711@gmail.com>
2025-12-10 22:13:05 +0900

    로그파일 재정렬

---
+++ coding_test/log_file.py
@@ -0,0 +1,20 @@
+
+
+def sort(list: list[str]) -> list[str]:
+    letters, digits = [], []
+    
+    # 1. 로그 분류
+    for log in list:
+        if log.split()[1].isdigit():
+            digits.append(log)
+        else:
+            letters.append(log)    
+
+    # 2. 문자 로그만 정렬
+    letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+
+    # 3. 문자 로그 + 숫자 로그(원래 순서) 반환
+    return letters + digits            
+
+
+print(sort(["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]))
